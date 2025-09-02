3e87f70033517ee93212edb178073ae20b16dd5b
ahemyu <hansovic7@gmail.com>
2025-09-02 22:42:52 +0200

    feat(frontend): deck view number of (due) flashcards and deckname; back to decks dashboard button

--- frontend/src/deck.js
+++ frontend/src/deck.js
@@ -13,33 +13,50 @@ async function apiGet(path) {
   return res.json();
 }
 
-
 // =====================
 // Page Logic
 // =====================
 
+async function displayDeckName(deckId){
+  const deck = await apiGet(`/decks/${deckId}`);
+  const deckName = deck.name;
+  document.getElementById("deck-name").textContent = `${deckName}`
+}
+
 // So we need to get all of the flashcards of the current deck, total and due (we need to do two calls to the backend)
 // for that we need to know which deck we are in right now (identified by the deckid=n query parameter of the current url)
-
 async function getNumberOfFlashcards() {
     const queryString = window.location.search; 
     const params = new URLSearchParams(queryString);
     if (params.has("deckId")){
         const deckId = params.get("deckId");
+        displayDeckName(deckId);
         // Now we have the deck ID and can just do the calls to the backenmd
         // first get all flashcards 
         const allFlashcards = await apiGet(`/decks/${deckId}/flashcards`);
         const numberofAllFlashcards = allFlashcards.length;
+        document.getElementById("all-flashcards").textContent = `${numberofAllFlashcards} total cards`
         
+        // then due flashcards
         const dueFlashcards = await apiGet(`/decks/${deckId}/flashcards?due=true`);
         const numberOfDueFlashcards = dueFlashcards.length;
-        console.log(numberOfDueFlashcards);
-
+        document.getElementById("due-flashcards").textContent = `${numberOfDueFlashcards} due cards`
     }    
-
 }
 
+async function startReview(){
+  //this just needs to link to review page basically which should display all due flashcards one after the other
+  document.getElementById("re
+    view-button").addEventListener("click", () => {
+    window.location.href = "review.html"; //TODO: Adjust this ya salame
+  });
+}
 
+async function backToDecks(){
+  document.getElementById("back-to-dashboard").addEventListener("click", () => {
+    window.location.href = "start.html";
+  });
+}
 
 // =====================
 // Init
@@ -52,6 +69,8 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   try {
     getNumberOfFlashcards();
+    startReview();
+    backToDecks();
 
   } catch (err) {
     console.error(err);
