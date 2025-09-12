3099326cbca9ce8b1fd8557f399952761c29f204
ahemyu <hansovic7@gmail.com>
2025-09-12 14:47:41 +0200

    feat(frontend): review flow almost finished

--- frontend/src/review.js
+++ frontend/src/review.js
@@ -1,5 +1,4 @@
 
-// =====================
 // API Helpers
 // =====================
 const authToken = localStorage.getItem("authToken");
@@ -28,20 +27,61 @@ async function apiPost(path, body) {
 // Page Logic
 // =====================
 
-// we need to do backend call to get the number of due flashcards
-
 async function getDueFlashcards(deckId){
     const dueFlashcards = await apiGet(`/decks/${deckId}/flashcards`);
-    //TODO: sort based on next_review_at
+    dueFlashcards.sort((a,b) => new Date(a.next_review_at) - new Date(b.next_review_at));
+
+    return dueFlashcards;
 }
 
-//TODO: eventhandler to show back once user clicks enter or the "Show Answer" button
+async function showAndHideBack(){
+    // add eventhanlder for the show answer button 
+    const showAnswerButton = document.getElementById("show-answer");
+    const backDiv = document.getElementById("back");
+    showAnswerButton.addEventListener("click", () => {
+        backDiv.classList.add("show");
+    });
+}
+
+async function fillFrontAndBack(){
+    const frontP = document.getElementById("front-p");
+    const backP = document.getElementById("back-p");
+    
+    // use the global currentIndex to determione which flahcrad we are looking at 
+    const currentFlashcard = reviewQueue[currentIndex];
+    frontP.textContent = currentFlashcard.front;
+    backP.textContent = currentFlashcard.back;
+}
+
+async function sendFeedback(){
+    // we need to get the curernt flashcard"s id (referencing the global variable) 
+    const badFeedbackButton = document.getElementById("bad");
+    const midFeedbackButton = document.getElementById("mid");
+    const goodFeedbackButton = document.getElementById("good");
+    const currentFlashcard = reviewQueue[currentIndex];
+
+    badFeedbackButton.addEventListener("click", async () => {
+        await apiPost(`/flashcards/${currentFlashcard.id}/review`, {"feedback": "bad"});
+        currentIndex++;
+    });
+   midFeedbackButton.addEventListener("click", async () => {
+        await apiPost(`/flashcards/${currentFlashcard.id}/review`, {"feedback": "mid"});
+        currentIndex++;
+    });
+    goodFeedbackButton.addEventListener("click", async () => {
+        await apiPost(`/flashcards/${currentFlashcard.id}/review`, {"feedback": "good"});
+        currentIndex++;
+    });
+
+}   
 
 // =====================
 // Init
 // =====================
+// =====================
 
-
+let reviewQueue = [];
+let currentIndex = 0;
 document.addEventListener("DOMContentLoaded", async () => {
   if (!authToken) {
     window.location.href = "login.html";
@@ -51,8 +91,11 @@ document.addEventListener("DOMContentLoaded", async () => {
     const queryString = window.location.search; 
     const params = new URLSearchParams(queryString);
     const deckId = params.get("deckId");
-    await getDueFlashcards(deckId);
-
+    await showAndHideBack();
+    reviewQueue =  await getDueFlashcards(deckId);
+    await fillFrontAndBack();
+    await sendFeedback()
+    // TODO need to call the function that willl populate front and back accordingly based on current flashcard 
   } catch (err) {
     console.error(err);
   }
